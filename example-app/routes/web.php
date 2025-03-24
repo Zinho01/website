21c87efcd9d010ab8fe345dee1df7c288f6ee9e7
Roald <nihilzero28@gmail.com>
2025-03-24 14:35:15 +0100

    adding many files

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('index');  // Returns the 'index.blade.php' view
+});
+
+Route::get('/huid', function () {
+    return view('huid');  
+});
+
+Route::get('/acu', function () {
+    return view('acu');  
+});
+
+Route::get('/tarieven', function () {
+    return view('tarieven');  
+});
+
+Route::get('/about', function () {
+    return view('about');  
+});
+
+Route::get('/contact', function () {
+    return view('contact');  
+});
+
+Route::get('/shop', function () {
+    return view('shop');  
+});
