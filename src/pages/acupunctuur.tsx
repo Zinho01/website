daa16ec4e8f18801027c7683b564b437e733efca
Akshay12184 <144685658+Akshay12184@users.noreply.github.com>
2025-05-06 12:19:47 +0200

    changed the linking to an agenda system on all pages

--- src/pages/acupunctuur.tsx
+++ src/pages/acupunctuur.tsx
@@ -35,7 +35,7 @@ const Acupunctuur: React.FC = () => {
           </ul>
         </div>
 
-        <a href="/contact" className="inline-block mt-6">
+        <a href="https://the-beauty-clinic-3.salonized.com/widget_bookings/new" target="_blank" rel="noopener noreferrer" className="inline-block mt-6">
           <img src={acupunctuur} alt="foto-acupunctuur" className="w-full max-w-sm mx-auto mb-5 rounded-lg" />
           <motion.button 
             className="bg-neutral-500 hover:bg-orange-200 text-white px-4 sm:px-6 py-2 sm:py-3 rounded-lg transition duration-300" whileHover={{ scale: 1.05 }}>
