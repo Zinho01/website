96b444077207cdc80be33443038028009f2429fd
Roald <nihilzero28@gmail.com>
2025-07-09 10:50:33 +0200

    Either way, destiny still arrives.

---
+++ websitewip/app/Models/OrderItem.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model {
+    protected $fillable = ['order_id', 'product_variant_id', 'quantity', 'unit_price'];
+
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function variant() {
+        return $this->belongsTo(ProductVariant::class);
+    }
+}
+
+?>
--- websitewip/routes/api.php
+++ websitewip/routes/api.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
+
 use App\Http\Controllers\Api\ProductVariantController;
 use App\Http\Controllers\Api\ProductImageController;
+
+use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\CheckoutController;
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
@@ -15,5 +19,10 @@ Route::get('products/{product}/images', [ProductImageController::class, 'index']
 Route::post('products/{product}/images', [ProductImageController::class, 'store']);
 Route::delete('images/{image}', [ProductImageController::class, 'destroy']);
 Route::post('images/{image}/set-main', [ProductImageController::class, 'setMain']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('cart-items', CartController::class)->except(['show', 'edit', 'create']);
+    Route::post('checkout', [CheckoutController::class, 'placeOrder']);
+});
 
 ?>
