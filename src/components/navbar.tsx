371f69786167957bc5b80737b5776f50ccbbf85b
Akshay12184 <144685658+Akshay12184@users.noreply.github.com>
2025-07-17 11:33:34 +0200

    fixed issue with burger menu not closing when swapping to differet pages

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import lotusImage from '../assets/lotus.png';
 
 const Navbar: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const location = useLocation();
 
     const toggleMenu = () => setIsOpen(!isOpen);
+
+    useEffect(() => {
+        setIsOpen(false);
+    }, [location.pathname]);
 
     return (
         <nav className="relative bg-white p-4">
@@ -35,9 +40,9 @@ const Navbar: React.FC = () => {
                 <div className="lg:hidden flex items-center ml-4">
                     <button onClick={toggleMenu} className="text-black focus:outline-none">
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                             xmlns="http://www.w3.org/2000/svg">
+                            xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M4 6h16M4 12h16M4 18h16"/>
+                                d="M4 6h16M4 12h16M4 18h16"/>
                         </svg>
                     </button>
                 </div>
